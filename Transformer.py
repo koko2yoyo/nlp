1d67d0420b9cf9b5a7a66f51251372c47d9a082e
koko2yoyo <K15829600903@163.com>
2025-06-28 10:32:09 +0800

    Update Transformer.py

--- Transformer.py
+++ Transformer.py
@@ -1,14 +1,10 @@
 import torch
 import 
 
-# transformer encoder 主要由attention+残差层+dropout+两层线性层+dropout组成
-class mutli_head_attention(nn.Module):
-  def __init__(self,d_model,head_num,head_size):
-    super().__init__()
-    self.d_model =d_model
-    self.head_num = head_num
-    self.head_size =head_size
-    self.Wq = nn.linear(self.d_model,self.head_num*)
+# Transformer主要由Encoder和Decoder组成
+# Encoder:MutilHeadAttention + PoswiseFeedForward
+
+class
     
 class Transformer_encoder(nn.Module):
   def __init__(self,d_model,head_num,head_size):
